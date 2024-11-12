36859d3b250968e7f687f32806716447568ff1f4
Venkatesh <venkateshpk06@gmail.com>
2024-11-12 21:27:31 +0530

    update

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ const User = require('./models/user');
 
 const Auth = {
     isAuth: async (req, res, next) => {
-        const token = req.cookies.token || req.headers['authorization']?.split(' ')[1];
+        const token = req.cookies.token;
         if (!token) {
             return res.status(401).json({ message: "Unauthorized" });
         }
